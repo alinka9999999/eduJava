ec2bdf445e57ce27d173f4ba0a395651a6e893c1
Alina Yakhina <ayahina@cmx.ru>
2021-02-01 09:56:49 +0300

    добавлено удаление группы

--- addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationsTests.java
@@ -1,37 +1,9 @@
 package ru.addressbook;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class GroupCreationsTests {
-    private WebDriver wd;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        initGroupCreation("user");
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        initGroupCreation("pass");
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.id("LoginForm")).submit();
-    }
+public class GroupCreationsTests extends TestBase {
 
     @Test
     public void testGroupCreations() throws Exception {
@@ -42,50 +14,6 @@ public class GroupCreationsTests {
         fillGroupForm(new GroupData("test", "test", "test"));
         initGroupCreation("submit");
         goTogroup();
-        wd.findElement(By.linkText("Logout")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
     }
 
-    private void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        initGroupCreation("group_header");
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        initGroupCreation("group_footer");
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation(String s) {
-        wd.findElement(By.name(s)).click();
-    }
-
-    private void goTogroup() {
-        wd.findElement(By.linkText("groups")).click();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }

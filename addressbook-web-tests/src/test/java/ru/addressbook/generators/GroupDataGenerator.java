277a262c5c2335dcde6adfde82db5bda3ae29c09
Alina Yakhina <ayahina@cmx.ru>
2021-02-08 17:22:14 +0300

    6.8. Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/generators/GroupDataGenerator.java
@@ -43,35 +43,35 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         } else if (format.equals("json")) {
             saveAsJson(groups, new File(file));
-    } else {
+        } else {
             System.out.println("Unrecognized format");
-    }
+        }
 
-}
+    }
 
     private void saveAsJson(List<GroupData> groups, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(groups);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
-    private static List<GroupData> generateGroups ( int count){
-            List<GroupData> groups = new ArrayList<GroupData>();
-            for (int i = 0; i < count; i++) {
-                groups.add(new GroupData().withName(String.format("test %s", i)).
-                        withHeader((String.format("header %s", i))).
-                        withFooter((String.format("footer %s", i))));
-            }
-            return groups;
+    private static List<GroupData> generateGroups(int count) {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        for (int i = 0; i < count; i++) {
+            groups.add(new GroupData().withName(String.format("test %s", i)).
+                    withHeader((String.format("header %s", i))).
+                    withFooter((String.format("footer %s", i))));
         }
+        return groups;
+    }
 
-        private void saveAsCsv(List < GroupData > groups, File file) throws IOException {
-            Writer writer = new FileWriter(file);
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
+        try (Writer writer = new FileWriter(file)) {
             for (GroupData group : groups) {
                 writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
             }
-            writer.close();
         }
     }
+}
--- addressbook-web-tests/src/test/java/ru/addressbook/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/test/ContactCreationTest.java
@@ -27,29 +27,34 @@ public class ContactCreationTest extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
-        String line = reader.readLine();
-        while (line != null){
-            String[] split = line.split(";");
-            list.add(new Object[] {new ContactData().withName(split[0]).withLastname(split[1]).withAddress(split[2])});
-            line = reader.readLine();
-        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")))) {
+            String line = reader.readLine();
+        while(line !=null)
+
+            {
+                String[] split = line.split(";");
+                list.add(new Object[]{new ContactData().withName(split[0]).withLastname(split[1]).withAddress(split[2])});
+                line = reader.readLine();
+            }
         return list.iterator();
+        }
     }
 
     @DataProvider
     public Iterator<Object[]> validContactsFromJson() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null){
-            json += line;
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<GroupData> contacts = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+            }.getType());
+            return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<GroupData> contacts = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
-        return contacts.stream().map((g)->new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
     @BeforeMethod

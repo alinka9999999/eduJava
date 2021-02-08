7b8425df0e21de12cb41aaafb3b291ccf05c18a8
Alina Yakhina <ayahina@cmx.ru>
2021-02-08 08:43:04 +0300

    11 задание

--- addressbook-web-tests/src/test/java/ru/addressbook/test/ContactAddressTest.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/test/ContactAddressTest.java
@@ -4,13 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.addressbook.model.ContactData;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
-public class ContactEmailsTest extends TestBase{
+public class ContactAddressTest extends TestBase{
 
     @BeforeMethod
     public void ensurePreconditions() {
@@ -23,19 +20,12 @@ public class ContactEmailsTest extends TestBase{
         }
     }
 
-    
     @Test
-    public void testContactEmails () {
+    public void testContactPhones() {
         ContactData contact = app.contact().allContacts().iterator().next();
-        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
-
-    }
-
-    private String mergeEmails(ContactData contact) {
-        return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
-                .stream().filter((s) -> ! s.equals(""))
-                .map(ContactPhoneTest::cleaned)
-                .collect(Collectors.joining("\n"));
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm (contact);
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
     }
 }
+
+

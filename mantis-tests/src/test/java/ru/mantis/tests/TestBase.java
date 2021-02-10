45491cfc313805660403f34aac1688d2e26a6c5c
Alina Yakhina <ayahina@cmx.ru>
2021-02-10 07:18:49 +0300

    17 задание

---
+++ mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.mantis.tests.appmanager.ApplicationManager;
+
+public class TestBase {
+    static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
+
---
+++ mantis-tests/src/test/java/ru/mantis/tests/appmanager/ApplicationManager.java
@@ -0,0 +1,32 @@
+package ru.mantis.tests.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private String browser;
+
+    public ApplicationManager(String browser) {
+     this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+}

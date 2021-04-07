beb589b577302b13c67bec48a5c25debb41a5b07
Alina Yakhina <ayahina@cmx.ru>
2021-04-07 16:32:04 +0300

    добавлено для тестов

--- addressbook-web-tests/src/test/java/ru/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/test/TestBase.java
@@ -22,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
     static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 

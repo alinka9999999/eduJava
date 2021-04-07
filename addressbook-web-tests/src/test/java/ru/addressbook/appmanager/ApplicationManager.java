f2e80db446dd57c7a38bccb3d353868b645f924c
Alina Yakhina <ayahina@cmx.ru>
2021-04-07 17:05:56 +0300

    10.2. Удалённый запуск браузера (Selenium Server)

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,11 +40,20 @@ public class ApplicationManager {
         System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
 
         dbHelper = new DbHelper();
+
+        if("".equals((properties.getProperty("selenium.server")))) {
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {wd = new InternetExplorerDriver();}
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();}
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);}
+        else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd =new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities)
+        }
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
